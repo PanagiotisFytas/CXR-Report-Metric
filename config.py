ce59a102759b176acc93dcd0d90f53b83cd6aa51
Panagiotis Fytas <p.fytas@datahow.ch>
2024-12-11 20:37:30 +0000

    config for multiple files

--- config.py
+++ config.py
@@ -3,9 +3,24 @@ CHEXBERT_PATH = "/mnt/nas_home/pf376/Documents/CXR-Report-Metric/chexbert/chexbe
 RADGRAPH_PATH = "/mnt/nas_home/pf376/Documents/CXR-Report-Metric/radgraph/model_checkpoint/model.tar.gz"
 
 # Report paths
-GT_REPORTS = "reports/gt_reports.csv"
-PREDICTED_REPORTS = "reports/predicted_reports.csv"
-OUT_FILE = "report_scores.csv"
+GT_REPORTS_FILES = [
+    "/mnt/nas_home/pf376/Documents/llama-rec/mimic_impression_model_Llama-3.2-1B-Instruct_peft_3.2-1B_mimic_impression_background_quantization_no_quantization_mode_test_outputs.csv",
+    "/mnt/nas_home/pf376/Documents/llama-rec/mimic_impression_model_Llama-3.2-1B-Instruct_peft_3.2-1B_mimic_impression_background_quantization_no_quantization_mode_val_outputs.csv",
+    "/mnt/nas_home/pf376/Documents/llama-rec/mimic_impression_model_Llama-3.2-1B-Instruct_peft_3.2-1B_mimic_impression_quantization_no_quantization_mode_test_outputs.csv",
+    "/mnt/nas_home/pf376/Documents/llama-rec/mimic_impression_model_Llama-3.2-1B-Instruct_peft_3.2-1B_mimic_impression_quantization_no_quantization_mode_val_outputs.csv",
+]
+PREDICTED_REPORTS_FILES = [
+    "/mnt/nas_home/pf376/Documents/llama-rec/mimic_impression_peft_3.2-1B_mimic_impression_background_mode_test_gold_outputs.csv",
+    "/mnt/nas_home/pf376/Documents/llama-rec/mimic_impression_peft_3.2-1B_mimic_impression_background_mode_val_gold_outputs.csv",
+    "/mnt/nas_home/pf376/Documents/llama-rec/mimic_impression_peft_3.2-1B_mimic_impression_mode_test_gold_outputs.csv",
+    "/mnt/nas_home/pf376/Documents/llama-rec/mimic_impression_peft_3.2-1B_mimic_impression_mode_val_gold_outputs.csv",
+]
+OUT_FILE_FILES = [
+    "/mnt/nas_home/pf376/Documents/CXR-Report-Metric/scores/mimic_impression_model_Llama-3.2-1B-Instruct_peft_3.2-1B_mimic_impression_background_quantization_no_quantization_mode_test_scores.csv",
+    "/mnt/nas_home/pf376/Documents/CXR-Report-Metric/scores/mimic_impression_model_Llama-3.2-1B-Instruct_peft_3.2-1B_mimic_impression_background_quantization_no_quantization_mode_val_scores.csv",
+    "/mnt/nas_home/pf376/Documents/CXR-Report-Metric/scores/mimic_impression_model_Llama-3.2-1B-Instruct_peft_3.2-1B_mimic_impression_quantization_no_quantization_mode_test_scores.csv",
+    "/mnt/nas_home/pf376/Documents/CXR-Report-Metric/scores/mimic_impression_model_Llama-3.2-1B-Instruct_peft_3.2-1B_mimic_impression_quantization_no_quantization_mode_val_scores.csv",
+]
 
 # Whether to use inverse document frequency (idf) for BERTScore
 USE_IDF = False
--- test_metric.py
+++ test_metric.py
@@ -2,10 +2,11 @@ import config
 from CXRMetric.run_eval import calc_metric
 from CXRMetric.run_eval import CompositeMetric
 
-gt_reports = config.GT_REPORTS
-predicted_reports = config.PREDICTED_REPORTS
-out_file = config.OUT_FILE
+gt_reports_files = config.GT_REPORTS
+predicted_reports_files = config.PREDICTED_REPORTS
+out_files = config.OUT_FILE
 use_idf = config.USE_IDF
 
 if __name__ == "__main__":
-    calc_metric(gt_reports, predicted_reports, out_file, use_idf)
+    for gt_reports, predicted_reports, out_file in zip(gt_reports_files, predicted_reports_files, out_files):
+        calc_metric(gt_reports, predicted_reports, out_file, use_idf)
