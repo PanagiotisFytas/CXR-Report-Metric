5cd3d35dabb6f7f0eb7c16a586facb95cb196571
Panagiotis Fytas <p.fytas@datahow.ch>
2025-04-14 16:14:44 +0100

    remove int

---
+++ print_avg_stats.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+OUT_FILE_FILES = [
+    "/mnt/nas_home/pf376/Documents/CXR-Report-Metric/scores/mimic_impression_model_Llama-3.2-1B-Instruct_peft_3.2-1B_mimic_impression_background_quantization_no_quantization_mode_test_scores.csv",
+    "/mnt/nas_home/pf376/Documents/CXR-Report-Metric/scores/mimic_impression_model_Llama-3.2-1B-Instruct_peft_3.2-1B_mimic_impression_background_quantization_no_quantization_mode_val_scores.csv",
+    "/mnt/nas_home/pf376/Documents/CXR-Report-Metric/scores/mimic_impression_model_Llama-3.2-1B-Instruct_peft_3.2-1B_mimic_impression_quantization_no_quantization_mode_test_scores.csv",
+    "/mnt/nas_home/pf376/Documents/CXR-Report-Metric/scores/mimic_impression_model_Llama-3.2-1B-Instruct_peft_3.2-1B_mimic_impression_quantization_no_quantization_mode_val_scores.csv",
+]
+
+# Load the CSV files into DataFrames
+df_list = [pd.read_csv(file) for file in OUT_FILE_FILES]
+
+# print the average of each column for each DataFrame
+for i, df in enumerate(df_list):
+    print(f"file: {OUT_FILE_FILES[i]}")
+    print(f"DataFrame {i + 1} average:")
+    print(df.mean())
+    print("\n")
