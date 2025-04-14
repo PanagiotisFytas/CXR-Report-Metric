ed4f7b874ed3ab49cb0a0e10659d70805d57d4ea
Panagiotis Fytas <p.fytas@datahow.ch>
2025-04-14 16:17:19 +0100

    remove int

--- print_avg_stats.py
+++ print_avg_stats.py
@@ -7,6 +7,13 @@ OUT_FILE_FILES = [
     "/mnt/nas_home/pf376/Documents/CXR-Report-Metric/scores/mimic_impression_model_Llama-3.2-1B-Instruct_peft_3.2-1B_mimic_impression_quantization_no_quantization_mode_val_scores.csv",
 ]
 
+OUT_FILE_NAMES = [
+    "background_test_scores",
+    "background_val_scores",
+    "test_scores",
+    "val_scores",
+]
+
 # Load the CSV files into DataFrames
 df_list = [pd.read_csv(file) for file in OUT_FILE_FILES]
 
